fd52f01ea8bf3ea4ef1523c6f5c51fb035b4d2d7
TM <tonimaxx@gmail.com>
2021-10-15 09:51:16 -0700

    Add Controllers

--- tmpyclick.py
+++ tmpyclick.py
@@ -1,5 +1,10 @@
 from pynput.keyboard import Key, Controller as KeyboardController
 from pynput.mouse import Button, Controller as MouseController
 
-print("Hello TM")
 
+
+keyboard = KeyboardController()
+mouse = MouseController()
+
+
+print("Hello TM")
